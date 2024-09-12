48c057c0e981761582fdfe515830bb7a7c52d354
Oliver Hubert <hubertlion@aol.com>
2024-09-12 09:35:35 +0200

    Projektdateien hinzufügen.

---
+++ LoggerViewer/Models/LogEntry.cs
@@ -0,0 +1,9 @@
+﻿namespace LoggerViewer.Models
+{
+    public class LogEntry
+    {
+        public int Id { get; set; }
+        public string Mpn { get; set; }
+        public string Manufacturer { get; set; }
+    }
+}
---
+++ LoggerViewer/Program.cs
@@ -0,0 +1,12 @@
+using LoggerViewer;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7026/") });
+
+
+await builder.Build().RunAsync();
